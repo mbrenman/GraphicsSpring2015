ecc23718c3355846e9980df242ab4ea54e3246ab
MattRC7 <mattrc7@comcast.net>
2015-02-26 20:19:14 -0500

    "figured some stuff out"

--- Lab3/solarSystem.cpp
+++ Lab3/solarSystem.cpp
@@ -14,21 +14,24 @@ solarSystem::~solarSystem(){
 // for each of the planets with parameters.
 void solarSystem::render(){
 	
-	// Some ideas for constants that can be used for
+	// Some ideas for variables that can be used for
 	// rotating the planets.
 	static float rotation = 0.1;
 	static float orbitX = 0.15;
-	static float orbitSpeed = 0.2;
+	static float orbitSpeed = 0.1;
 	static float moonOrbitX = 0.1;
 	static float moonOrbitY = 0.1;
-	static float moonOrbitSpeed = 1;
+	static float moonOrbitSpeed = 0.2;
 
 	// The Sun
 		glPushMatrix();
-			glRotatef(rotation,0,1,0);
+			renderPlanet(3, orbitX, 0.2, rotation * 2);
+			renderPlanet(5, orbitX * 0.5, 0.6, rotation * 1.5);
+			glRotatef(rotation, 0, 1, 0);
 			glColor3f(0.96f,0.85f,0.26f);
 			glutSolidSphere(1.2,10,10);
 		glPopMatrix();
+
 	// Add more planets, moons, and rings here!	
 		
 
@@ -37,4 +40,25 @@ void solarSystem::render(){
 	moonOrbitX+=moonOrbitSpeed;
 	moonOrbitY+=moonOrbitSpeed;
 
+}
+
+void solarSystem::renderPlanet(float orbitrad, float orbitdeg, float size, float rotdeg) {
+	glPushMatrix();
+	glRotatef(orbitdeg, 0, 1, 0);
+	glTranslatef(orbitrad, 0, 0);
+	renderMoon(orbitrad*0.3, orbitdeg*0.5, size*0.3, rotdeg);
+	glRotatef(rotdeg, 0, 1, 0);
+	glColor3f(0.26f, 0.26f, 0.96f);
+	glutSolidSphere(size, 10, 10);
+	glPopMatrix();
+}
+
+void solarSystem::renderMoon(float orbitrad, float orbitdeg, float size, float rotdeg) {
+	glPushMatrix();
+	glRotatef(orbitdeg, 0, 1, 0);
+	glTranslatef(orbitrad, 0, 0);
+	glRotatef(rotdeg, 0, 1, 0);
+	glColor3f(0.26f, 0.96f, 0.26f);
+	glutSolidSphere(size, 10, 10);
+	glPopMatrix();
 }
