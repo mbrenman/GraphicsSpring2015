f23852b31a8062127285f707e9efbf9804a512ce
MattRC7 <mattrc7@comcast.net>
2015-03-09 18:49:40 -0400

    Copied code from A1 shape headers into A2 headers

--- A2/Cube.h
+++ A2/Cube.h
@@ -1,19 +1,99 @@
-#ifndef CUBE_H
-#define CUBE_H
+#ifndef COHE_H
+#define CONE_H
 
 #include "Shape.h"
 
-class Cube : public Shape {
+class Cone : public Shape {
 public:
-	Cube() {};
-	~Cube() {};
+	Cone() {};
+	~Cone() {};
 
 	void draw() {
+		for (int i = 0; i < body.size(); i++) {
+			drawPart(body.at(i), GL_TRIANGLE_STRIP, false);
+		}
+		drawPart(base, GL_TRIANGLE_FAN, false);
 	};
-
 
 	void drawNormal() {
+		for (int i = 0; i < body.size(); i++) {
+			drawPart(body.at(i), GL_TRIANGLE_STRIP, true);
+		}
+		drawPart(base, GL_TRIANGLE_FAN, true);
 	};
+
+	void recompute() {
+		std::cout << "redrawing!" << std::endl;
+		//clear Faces
+		for (int i = 0; i < body.size(); i++) {
+			body[i].clear();
+		}
+		body.clear();
+
+		m_rmatrix = rot_mat(Vector(0.0, 1.0, 0.0), -(2 * PI) / m_segmentsX);
+
+		Point startp(0.5, -0.5, 0.0);
+		Point nextp(0.5*cos(2 * PI / m_segmentsX), -0.5, 0.5*sin(2 * PI / m_segmentsX));
+		Point topp(0.0, 0.5, 0.0);
+		computeBody(startp, nextp, topp, body);
+		computeBase(Point(0.0, -0.5, 0.0), startp, nextp, base, Vector(0.0, -1.0, 0.0));
+	}
+private:
+	void computeBody(Point startp, Point nextp, Point topp, std::vector< std::vector<point_info> > &body) {
+		Vector rot_vec = nextp - startp;
+		Vector tess_vec1, tess_vec2;
+
+		for (int i = 0; i < m_segmentsX; i++) {
+			tess_vec1 = (topp - startp) / m_segmentsY;
+			tess_vec2 = (topp - nextp) / m_segmentsY;
+
+			std::vector< point_info > face;
+			computeStrip(startp, nextp, tess_vec1, tess_vec2, face);
+			startp = nextp;
+			rot_vec = m_rmatrix * rot_vec;
+			nextp = nextp + rot_vec;
+			body.push_back(face);
+		}
+	};
+
+	void computeStrip(Point startp, Point nextp, Vector tess_vec1, Vector tess_vec2, std::vector<point_info> &face){
+		for (int i = 0; i <= m_segmentsY; i++) {
+			if (i == m_segmentsY) {
+				face.push_back(point_info(startp, Vector(0, 0, 0), false));
+				face.push_back(point_info(nextp, Vector(0, 0, 0), true));
+			}
+			else {
+				face.push_back(point_info(startp, coneBodyVector(startp)));
+				face.push_back(point_info(nextp, coneBodyVector(nextp)));
+			}
+			startp = startp + tess_vec1;
+			nextp = nextp + tess_vec2;
+		}
+	};
+
+	Vector coneBodyVector(Point p) {
+		Vector normal = Vector(p.at(0), 0, p.at(2));
+		normal.normalize();
+		normal = Vector(normal.at(0), 0.5, normal.at(2));
+		normal.normalize();
+		return normal;
+	}
+
+	void computeBase(Point centerpt, Point edgept1, Point edgept2, std::vector<point_info> &base, Vector normal){
+		normal.normalize();
+		base.clear();
+		base.push_back(point_info(centerpt, normal));
+		Vector rot_vec = edgept2 - edgept1;
+		for (int i = 0; i <= m_segmentsX; i++){
+			base.push_back(point_info(edgept1, normal, i == m_segmentsX ? true : false));
+			edgept1 = edgept1 + rot_vec;
+			rot_vec = m_rmatrix * rot_vec;
+		}
+	};
+
+	std::vector< std::vector<point_info> > body;
+	std::vector< point_info > base;
+	Matrix m_rmatrix;
 };
 
 #endif
--- A2/Cylinder.h
+++ A2/Cylinder.h
@@ -3,15 +3,90 @@
 
 #include "Shape.h"
 
+
 class Cylinder : public Shape {
 public:
 	Cylinder() {};
 	~Cylinder() {};
 
 	void draw() {
+		for (int i = 0; i < body.size(); i++) {
+			drawPart(body.at(i), GL_TRIANGLE_STRIP, false);
+		}
+		drawPart(caps[TOP], GL_TRIANGLE_FAN, false);
+		drawPart(caps[BOTTOM], GL_TRIANGLE_FAN, false);
 	};
 
 	void drawNormal() {
+		for (int i = 0; i < body.size(); i++) {
+			drawPart(body.at(i), GL_TRIANGLE_STRIP, true);
+		}
+		drawPart(caps[TOP], GL_TRIANGLE_FAN, true);
+		drawPart(caps[BOTTOM], GL_TRIANGLE_FAN, true);
 	};
+
+	void recompute() {
+		std::cout << "redrawing!" << std::endl;
+		//clear Faces
+		for (int i = 0; i < body.size(); i++) {
+			body[i].clear();
+		}
+		body.clear();
+
+		m_rmatrix = rot_mat(Vector(0.0, 1.0, 0.0), -(2 * PI) / m_segmentsX);
+
+		Point startp(0.5, -0.5, 0.0);
+		Point nextp(0.5*cos(2 * PI / m_segmentsX), -0.5, 0.5*sin(2 * PI / m_segmentsX));
+		computeBody(startp, nextp - startp, Vector(0.0, 1.0 / m_segmentsY, 0.0), body);
+
+		computeCap(Point(0.0, -0.5, 0.0), startp, nextp, caps[BOTTOM], Vector(0.0, -1.0, 0.0));
+		computeCap(Point(0.0, 0.5, 0.0), startp + Vector(0.0, 1.0, 0.0),
+			nextp + Vector(0.0, 1.0, 0.0), caps[TOP], Vector(0.0, 1.0, 0.0));
+	}
+private:
+	enum CYL_CAPS { TOP, BOTTOM, NUM_CAPS };
+
+	void computeBody(Point startp, Vector rot_vec, Vector tess_vec, std::vector< std::vector<point_info> > &body) {
+		for (int i = 0; i < m_segmentsX; i++) {
+			std::vector< point_info > face;
+			computeStrip(startp, rot_vec, tess_vec, face);
+			startp = startp + rot_vec;
+			rot_vec = m_rmatrix * rot_vec;
+			body.push_back(face);
+		}
+	};
+
+	void computeStrip(Point startp, Vector vert_vec, Vector horiz_vec, std::vector<point_info> &face){
+		Point p = startp;
+		for (int i = 0; i <= m_segmentsY; i++) {
+			face.push_back(point_info(p, cylBodyVector(p), false));
+			p = p + vert_vec;
+			face.push_back(point_info(p, cylBodyVector(p), i == m_segmentsY ? true : false));
+			p = p - vert_vec;
+			p = p + horiz_vec;
+		}
+	};
+
+	Vector cylBodyVector(Point p) {
+		Vector normal = p - Point(0, p.at(1), 0);
+		normal.normalize();
+		return normal;
+	}
+
+	void computeCap(Point centerpt, Point edgept1, Point edgept2, std::vector<point_info> &cap, Vector normal){
+		normal.normalize();
+		cap.clear();
+		cap.push_back(point_info(centerpt, normal, false));
+		Vector rot_vec = edgept2 - edgept1;
+		for (int i = 0; i <= m_segmentsX; i++){
+			cap.push_back(point_info(edgept1, normal, i == m_segmentsX ? true : false));
+			edgept1 = edgept1 + rot_vec;
+			rot_vec = m_rmatrix * rot_vec;
+		}
+	};
+
+	std::vector< std::vector<point_info> > body;
+	std::vector< point_info > caps[NUM_CAPS];
+	Matrix m_rmatrix;
 };
 #endif
